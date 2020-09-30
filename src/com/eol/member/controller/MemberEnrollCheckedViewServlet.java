116dc7040e5092b62955e21860a0a7254b886e38
YOSEPH <laksmi0628@gmail.com>
2020-09-30 17:49:50 +0900

    #12 로그인 화면 및 기능 구현 -ys-

--- src/com/eol/member/controller/MemberEnrollCheckedViewServlet.java
+++ src/com/eol/member/controller/MemberEnrollCheckedViewServlet.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class memberEnrollCheckedViewServlet
  */
 @WebServlet("/memberEnrollCheckedView.do")
-public class memberEnrollCheckedViewServlet extends HttpServlet {
+public class MemberEnrollCheckedViewServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public memberEnrollCheckedViewServlet() {
+    public MemberEnrollCheckedViewServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
--- src/com/eol/member/model/dao/MemberDao.java
+++ src/com/eol/member/model/dao/MemberDao.java
@@ -1,14 +1,16 @@
 package com.eol.member.model.dao;
 
+import static com.eol.common.JDBCTemplate.close;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 
 import com.eol.member.model.vo.Member;
-import static com.eol.common.JDBCTemplate.close;
 
 public class MemberDao {
 	
@@ -21,6 +23,40 @@ public class MemberDao {
 		}catch(IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public Member selectMember(Connection conn, String userId, String password) {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		Member m = null;
+		try {
+			pstmt = conn.prepareStatement(prop.getProperty("selectMember"));
+			pstmt.setString(1, userId);
+			pstmt.setString(2, password);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				m = new Member();
+				m.setmNo(rs.getInt("mNo"));
+				m.setmId(rs.getString("mId"));
+				m.setmPw(rs.getString("mPw"));
+				m.setmPw(rs.getString("mName"));
+				m.setmBirth(rs.getDate("mBirth"));
+				m.setmEmail(rs.getString("mEmail"));
+				m.setmPhone(rs.getString("mPhone"));
+				m.setmAddress(rs.getString("mAddress"));
+				m.setmGender(rs.getString("mGender"));
+				m.setmLevel(rs.getInt("mLevel"));
+				m.setmBuy(rs.getInt("mBuy"));
+				m.setmDrop(rs.getInt("mDrop"));
+				m.setmEdate(rs.getDate("mEdate"));
+				m.setmPonint(rs.getInt("mPoint"));
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(rs);
+			close(pstmt);
+		}return m;
 	}
 	
 	public int insertMember(Connection conn, Member m) {
@@ -48,5 +84,6 @@ public class MemberDao {
 		}
 		return result;
 	}
+	
 
 }
