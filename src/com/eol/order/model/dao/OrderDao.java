120c48cb49f9fcb00a570cc2b3ebf8128085da48
yoonna <yoonnable@gmail.com>
2020-10-06 15:16:59 +0900

    #08 비회원기능구현1 헤더 버튼 화면전환기능 구현 -yn-

--- src/com/eol/order/model/dao/OrderDao.java
+++ src/com/eol/order/model/dao/OrderDao.java
@@ -30,7 +30,7 @@ public class OrderDao {
 		Orders o = null;
 		try {
 			pstmt = conn.prepareStatement(prop.getProperty("nNOrderselect"));
-					//nNOrderselect=SELECT * FROM ORDERS WHERE M_NO=NULL AND O_NO=? AND O_PW=?
+			//nNOrderselect=SELECT * FROM ORDERS WHERE M_NO=999 AND O_NO=? AND O_PW=?
 			pstmt.setString(1,nMONum);
 			pstmt.setString(2, pw);
 			rs=pstmt.executeQuery();
@@ -38,13 +38,20 @@ public class OrderDao {
 				o = new Orders();
 				o.setoNo(rs.getInt("o_no"));
 				o.setmNo(rs.getInt("m_no"));
-				o.setoRdate(rs.getDate("o_rdate"));
+				o.setoRDate(rs.getDate("o_rdate"));
 				o.setoName(rs.getString("o_name"));
 				o.setoPhone(rs.getString("o_phone"));
 				o.setoAddress(rs.getString("o_address"));
 				o.setoDeliveryDate(rs.getDate("o_deliverydate"));
 				o.setoAmount(rs.getInt("o_amount"));
 				o.setoPayment(rs.getInt("o_payment"));
+				o.setoPw(rs.getString("o_pw"));
+				o.setoStatus(rs.getString("o_status"));
+				o.setoPayDate(rs.getDate("o_paydate"));
+				o.setoAllowDate(rs.getDate("o_allowdate"));
+				o.setoDeliveryStatus(rs.getString("o_deliverystatus"));
+				o.setoDeliveryEDate(rs.getDate("o_deliveryedate"));
+				o.setoPayWays(rs.getString("o_payways"));
 			}
 		}catch(SQLException e) {
 			e.printStackTrace();
